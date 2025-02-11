3580a38f342bae4a0d3f9808844f747708a2771d
rohit6919 <patelrohit6919@gmail.com>
2025-02-11 11:42:13 +0530

    bgColor change Project

---
+++ 04_bgchange/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+function App() {
+  const [Color, setColor] = useState('olive');
+
+  return (
+    <div
+      className="w-full h-full"
+      style={{ backgroundColor: Color, height: '100vh', width: '100vw' }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0'>
+        <div className='flex flex-wrap gap-3 shadow-lg bg-white rounded-5xl'>
+          <button 
+          onClick={()=>setColor("red")}
+          className='outline-none px-4 rounded-2xl'
+          style={{backgroundColor:"red"}}> RED </button>
+
+          <button
+          onClick={()=>setColor("green")} 
+          className='outline-none px-4 rounded-2xl'
+          style={{backgroundColor:"green"}}> green </button>
+          <button
+          onClick={()=>setColor("purple")}
+          className='outline-none px-4 rounded-2xl'
+          style={{backgroundColor:"purple"}}> purple </button>
+          <button 
+          onClick={()=>setColor("blue")}
+          className='outline-none px-4 rounded-2xl'
+          style={{backgroundColor:"blue"}}> blue </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

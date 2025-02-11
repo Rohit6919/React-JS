d8999dd1c3170dc51c4b2f0da58f078eb394b912
rohit6919 <patelrohit6919@gmail.com>
2025-02-11 19:29:15 +0530

    new changes

--- 07_ReactRouter/src/main.jsx
+++ 07_ReactRouter/src/main.jsx
@@ -8,6 +8,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //     {
@@ -36,6 +38,8 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='Github' element={<Github />} />
     </Route>
   )
 )

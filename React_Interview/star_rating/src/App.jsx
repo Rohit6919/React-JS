0809b364a05664e3335ec94345cb2043787c7cd4
rohit6919 <patelrohit6919@gmail.com>
2025-02-23 13:18:10 +0530

    machine coding

---
+++ React_Interview/star_rating/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Starrating from './components/starrating'
+
+function App() {
+  return (
+    <>
+    <Starrating />
+    </>
+  )
+}
+
+export default App
---
+++ React_Interview/star_rating/src/components/Starrating.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export default function Starrating({StarCount = 5}){
+   const [values,setvalues] = useState('')
+   const [hover , sethover] = useState();   
+  
+   return <div>
+    {new Array(StarCount).fill().map((value,index)=>{
+        return <span key={index}
+        className={(hover == 0 && index < values) || index < hover ? "gold" : ""}
+        onClick={()=> setvalues(index+1)}
+        onMouseEnter={()=>sethover(index+1)}
+        onMouseLeave={()=>sethover(0)}
+        >&#9733;</span>
+    })}
+    </div>
+}
